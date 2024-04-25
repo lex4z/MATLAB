aeb7d92115146a0c6f82b28d0e4db7c64ab27426
lex4z <78318213+lex4z@users.noreply.github.com>
2024-04-25 19:25:25 +0300

    Create getff.m

---
+++ getff.m
@@ -0,0 +1,30 @@
+function ans = getff(x,n,epsy)
+    if n == 1
+        f = @(x) sqrt(x);
+    end
+    if n == 2
+        f = @(x)sin(x)./x;
+    end
+    if n == 3
+        f = @(x) abs(x)+cos(x)*2;
+    end
+    if n == 4
+        f = @(x) 100*x.^-1;
+    end
+    if n == 5
+        f = @(x)cos(x./2).*exp(x);
+    end
+    if n == 6
+        f =  @(x)cosh(x);
+    end
+    if n == 7
+        f = @(x)10*sin(x./10);
+    end
+    if n == 8
+        f = @(x) cos(2*x/5-4);
+    end
+    if n == 9
+        f = @(x) 7*exp(-x.^66+2)-exp(x/66);
+    end
+    ans = f(x).*((2*rand(size(x))-1)*epsy/100+1);
+end
