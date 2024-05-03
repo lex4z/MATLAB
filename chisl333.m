dafe58e73b0a2805f05e291dde8aaa22db86bdac
lex4z <78318213+lex4z@users.noreply.github.com>
2024-05-03 14:37:15 +0300

    Update chisl333.m

--- chisl333.m
+++ chisl333.m
@@ -50,49 +50,51 @@ end
 writematrix(RES,"RES331.xlsx")
 
 %% 2.
+clear RES
+
 l = 1;
+
 
 for i = 1:size(H,3)    
     epsA = 1e-1;
-    P = (2*rand(N)-1)*epsA;
     A = H(:,:,i);
-    Ap = A*(eye(N)+P);
+    
     for j = 1:5
+        P = (2*rand(N)-1)*epsA;
+        Ap = A*(eye(N)+P);
         x0 = ceil(rand(N,1)*1000)/1000;
         b = A * x0;
-
+        
+        x = A\b;
+        err = norm(x-x0)/norm(x0);
+        r = norm(A*x-b);
+        ob = cond(A);
+        err_est = ob*r/norm(b);
+    
+        xp = Ap\b;
+        obp = cond(Ap);
+        rp = norm(Ap*xp-b);
+        errp = norm(x0-xp);
+        err_estp = obp*r/norm(b);
+        err_est2 = err_estp + norm(P);
+    
+        RES(l,1) = i;
+        RES(l,2) = cond(A);
+        RES(l,3) = err;
+        RES(l,4) = err_est;
+        RES(l,5) = norm(r);
+        RES(l,5) = epsA;
+        RES(l,6) = cond(Ap);
+        RES(l,7) = errp;
+        RES(l,8) = err_estp;
+        RES(l,9) = err_est2;
+        RES(l,10) = norm(rp);
+    
+        l = l + 1;
         epsA = epsA^2;
     end
     
     
-    
-
-    x = A\b;
-    err = norm(x-x0)/norm(x0);
-    r = norm(A*x-b);
-    ob = cond(A);
-    err_est = ob*r/norm(b);
-
-    xp = Ap\b;
-    obp = cond(Ap);
-    rp = norm(Ap*xp-b);
-    errp = norm(x0-xp);
-    err_estp = obp*r/norm(b);
-    err_est2 = err_estp + norm(P);
-
-    RES(l,1) = i;
-    RES(l,2) = cond(A);
-    RES(l,3) = err;
-    RES(l,4) = err_est;
-    RES(l,5) = norm(r);
-    RES(l,5) = epsA;
-    RES(l,6) = cond(Ap);
-    RES(l,7) = errp;
-    RES(l,8) = err_estp;
-    RES(l,9) = err_est2;
-    RES(l,10) = norm(rp);
-
-    l = l + 1;
 end
 
 writematrix(RES,"RES332.xlsx")
