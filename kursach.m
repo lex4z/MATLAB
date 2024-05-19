56628ffd26a2634370b0ba2b443f4c8f33312783
олегсий <alex181211@gmail.com>
2024-05-19 19:05:41 +0300

    Update kursach.m

--- kursach.m
+++ kursach.m
@@ -1,12 +1,29 @@
-f = @(x,y) x.^3 + x.*y +y.^3 -7./(x.*y);
-f2 = @(x) x.^2 - x;
+clear
+clc
+f = @(x,y) x.^3 + x.*y + y.^3 -7./(x.*y);
+f2 = @(x) x.^2 - x + sqrt(x);
 
-integral2(f,1,6,2,4.5)
-double_simpson(f,1,6,2,4.5,10000,10000)
-%integral(f2,0,5)
-%simpson(f2,0,5,1e7)
+%integral2(f,1,6,2,4.5)
+%double_simpson(f,1,6,2,4.5,10000,10000)
+integral(f2,0,5)
+simpson(f2,0,5,1e3)
 
-%plot(f2(linspace(0,5,100)))
+function result = simpson(f,a,b,n)
+n = n + mod(n,2);
+h = (b-a)/n;
+
+s = 0;
+t1 = f(a);
+
+for i = 1:2:n-1
+    t2 = f(a+i*h);
+    t3 = f(a+(i+1)*h);
+    s = s + t1 + 4*t2 + t3;
+    t1 = t3;
+end
+
+result = h/3*s;
+end
 
 function result = double_simpson(f, a, b, c, d, m, n)
 m = m + mod(m,2);
