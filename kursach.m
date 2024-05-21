5938927d59ac15db9ac7cb26fe4019d9c65cdbfb
олегсий <alex181211@gmail.com>
2024-05-21 16:52:27 +0300

    Update kursach.m

--- kursach.m
+++ kursach.m
@@ -1,27 +1,27 @@
 clear
 clc
 
-a = 2;
-b = 4.5;
-c = 1;
-d = 6;
+a = 1;
+b = 6;
+c = 2;
+d = 4.5;
 
 f = @(x,y) x.^3 + x.*y + y.^3 -7./(x.*y);
 f2 = @(x,y) x.^2 - x + sqrt(x);
 
-fprintf("%e\n",integral2(f,a,b,c,d))
-fprintf("%e\n",double_simpson(f,a,b,c,d,100,100))
-fprintf("%e\n",double_trapezoid(f,a,b,c,d,1000,1000))
-fprintf("%e\n",double_rectangle(f,a,b,c,d,1000,1000, 0))
-fprintf("%e\n",double_rectangle(f,a,b,c,d,1000,1000, 0.5))
-fprintf("%e\n",double_rectangle(f,a,b,c,d,1000,1000, 1))
+fprintf("%f\n",integral2(f,a,b,c,d))
+fprintf("%f\n",double_simpson(f,a,b,c,d,100,100))
+fprintf("%f\n",double_trapezoid(f,a,b,c,d,1000,1000))
+fprintf("%f\n",double_rectangle(f,a,b,c,d,1000,1000, 0))
+fprintf("%f\n",double_rectangle(f,a,b,c,d,1000,1000, 0.5))
+fprintf("%f\n",double_rectangle(f,a,b,c,d,1000,1000, 1))
 
-% integral(f2,0,5)
-% simpson(f2,0,5,1e3,0)
-% trapezoid(f2,0,5,1e3,0)
-% rectangle_int(f2,0,5,1e3,0,0)
-% rectangle_int(f2,0,5,1e3,0.5,0)
-% rectangle_int(f2,0,5,1e3,1,0)
+% fprintf("%f\n",integral(f2,0,5))
+% fprintf("%f\n",simpson(f2,0,5,1e3,0))
+% fprintf("%f\n",trapezoid(f2,0,5,1e5,0))
+% fprintf("%f\n",rectangle_int(f2,0,5,1e3,0,0))
+% fprintf("%f\n",rectangle_int(f2,0,5,1e3,0.5,0))
+% fprintf("%f\n",rectangle_int(f2,0,5,1e3,1,0))
 
 function result = simpson(f,a,b,n,y)
 n = n + mod(n,2);
@@ -62,7 +62,7 @@ end
 function result = trapezoid(f, a, b, n, y)
 h = (b-a)/n;
 s = 0.5*(f(a,y)+f(b,y));
-for i = 1:n
+for i = 1:n-1
     s = s + f(a + i*h,y);
 end
 
@@ -74,7 +74,7 @@ end
 function result = double_trapezoid(f, a, b, c, d, n, m)
 k = (d-c)/m;
 t = 0.5*(trapezoid(f,a,b,n,c) + trapezoid(f,a,b,n,d));
-for j = 1:m
+for j = 1:m-1
     t = t + trapezoid(f,a,b,n,c + j*k);
 end
 
@@ -102,7 +102,7 @@ k = (d-c)/m;
 t = 0;
 y = c + bias*k;
 
-for i = 1:n
+for i = 1:m
     t = t + rectangle_int(f,a,b,n,bias,y);
     y = y + k;
 end
